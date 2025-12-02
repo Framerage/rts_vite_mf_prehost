266d1f763c970e4fc1b6c4741ad4288064549edd
Artem Igonin <officialigonin@mail.ru>
2025-12-03 00:45:43 +0400

    test shared store text

--- src/pages/private_room/index.tsx
+++ src/pages/private_room/index.tsx
@@ -3,12 +3,18 @@ import styles from "./room.module.css";
 import {ErrorBoundary} from "react-error-boundary";
 import {FailedLoadContent} from "components/failedLoadContent";
 import DotLoader from "components/dotLoader";
+import {useAuthStore} from "store/authStore";
 
 const WssChat = lazy(() => import("chat_app/Chat"));
 export const PrivateRoom = () => {
+  const {actions} = useAuthStore();
   return (
     <div className={styles.roomWrapper}>
-      <div className={styles.roomContent}>Draw and chat</div>
+      <div className={styles.roomContent}>
+        Draw and chat
+        <button onClick={() => actions.setUser("Test")}>change name</button>
+      </div>
+
       <div className={styles.roomExtraContent}>
         <ErrorBoundary fallback={<FailedLoadContent />}>
           <Suspense
