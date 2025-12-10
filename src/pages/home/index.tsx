602b50a4e7e901544b43eb7d169bf069a9bc945f
Artem Igonin <officialigonin@mail.ru>
2025-12-10 16:31:48 +0400

    edit import vue

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,18 @@
-import {Suspense, useEffect, useRef} from "react";
+import {lazy, Suspense, useEffect, useRef} from "react";
 import styles from "./homepage.module.css";
 import DotLoader from "components/dotLoader";
 import {ErrorBoundary} from "react-error-boundary";
 import {FailedLoadContent} from "components/failedLoadContent";
 // const VueMainPage = lazy(() => import("child_main_page/SharedMainPage"));
-import SharedMainPage from "child_main_page/SharedMainPage";
-import {createApp} from "child_main_page/vue";
+const SharedMainPage = lazy(() => import("child_main_page/SharedMainPage"));
+// import SharedMainPage from "child_main_page/SharedMainPage";
+// await import {createApp} from "child_main_page/vue";
+const fetchVue = async () => (await import("child_main_page/vue")).default;
+const vue = await fetchVue();
 const VueComponentWrapper = () => {
   const divRef = useRef(null);
   useEffect(() => {
-    const component = createApp(SharedMainPage);
+    const component = vue?.createApp(SharedMainPage);
     component.mount(divRef.current);
     return () => {
       component.unmount();
