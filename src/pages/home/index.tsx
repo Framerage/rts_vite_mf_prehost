d0d6ff36ab4d887d1425c24b386502257a75d978
Artem Igonin <officialigonin@mail.ru>
2025-12-10 16:57:13 +0400

    clear load vue

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import {lazy, Suspense, useEffect, useRef} from "react";
+import {lazy, Suspense} from "react";
 import styles from "./homepage.module.css";
 import DotLoader from "components/dotLoader";
 import {ErrorBoundary} from "react-error-boundary";
@@ -7,20 +7,20 @@ import {FailedLoadContent} from "components/failedLoadContent";
 const SharedMainPage = lazy(() => import("child_main_page/SharedMainPage"));
 // import SharedMainPage from "child_main_page/SharedMainPage";
 // await import {createApp} from "child_main_page/vue";
-const fetchVue = async () => (await import("child_main_page/vue")).default;
-const vue = await fetchVue();
-const VueComponentWrapper = () => {
-  const divRef = useRef(null);
-  useEffect(() => {
-    const component = vue?.createApp(SharedMainPage);
-    component.mount(divRef.current);
-    return () => {
-      component.unmount();
-    };
-  });
+// const fetchVue = async () => (await import("child_main_page/vue")).default;
+// const vue = await fetchVue();
+// const VueComponentWrapper = () => {
+//   const divRef = useRef(null);
+//   useEffect(() => {
+//     const component = vue?.createApp(SharedMainPage);
+//     component.mount(divRef.current);
+//     return () => {
+//       component.unmount();
+//     };
+//   });
 
-  return <div ref={divRef}></div>;
-};
+//   return <div ref={divRef}></div>;
+// };
 const Home = () => {
   console.log("render home");
   return (
@@ -36,7 +36,7 @@ const Home = () => {
       </div>
       <ErrorBoundary fallback={<FailedLoadContent />}>
         <Suspense fallback={<div> vue is loading ... </div>}>
-          <VueComponentWrapper />
+          <SharedMainPage />
         </Suspense>
       </ErrorBoundary>
     </section>
